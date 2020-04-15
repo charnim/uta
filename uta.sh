5afa38f904a226e8978ee761bf393c09ec3c36a0
MilanC <charnim@pm.me>
2020-04-15 15:46:43 +0300

    Added recon-ng dns enumeration

--- uta.sh
+++ uta.sh
@@ -8,6 +8,52 @@ mkdir nmap exploits sqlrequests
 
 IFS='//' read -ra hostname <<< "$1"
 
+#var declarations
+
+domain="${hostname[2]}"
+path=$(pwd)
+stamp=$(date +"%m_%d_%Y")
+
+
+#recon ng dns enumeration start
+#create file with workspace.timestamp and start enumerating hosts
+touch $domain$stamp.resource
+echo ""
+echo "workspaces create $domain$stamp" >> $domain$stamp.resource
+echo ""
+echo "modules load recon/domains-hosts/bing_domain_web" >> $domain$stamp.resource
+echo "options set source $domain" >> $domain$stamp.resource
+echo "run" >> $domain$stamp.resource
+echo ""
+echo "modules load recon/domains-hosts/google_site_web" >> $domain$stamp.resource
+echo "options set source $domain" >> $domain$stamp.resource
+echo "run" >> $domain$stamp.resource
+echo ""
+echo "modules load recon/domains-hosts/netcraft" >> $domain$stamp.resource
+echo "options set source $domain" >> $domain$stamp.resource
+echo "run" >> $domain$stamp.resource
+echo ""
+echo "modules load recon/domains-hosts/brute_hosts" >> $domain$stamp.resource
+#echo "options set WORDLIST /root/go/src/amass/examples/wordlists/sorted_knock_dnsrecon_fierce_recon-ng.txt
+#" >> $domain$stamp.resource
+echo "options set source $domain" >> $domain$stamp.resource
+echo "run" >> $domain$stamp.resource
+echo ""
+echo "modules load recon/hosts-hosts/resolve" >> $domain$stamp.resource
+echo "options set source default" >> $domain$stamp.resource
+echo "run" >> $domain$stamp.resource
+echo ""
+echo "modules load reporting/list" >> $domain$stamp.resource
+echo "options set FILENAME $path/$domain$stamp.lst" >> $domain$stamp.resource
+echo "options set COLUMN host" >> $domain$stamp.resource
+echo "run" >> $domain$stamp.resource
+echo ""
+echo "exit" >> $domain$stamp.resource
+echo ""
+
+recon-ng -r $path/$domain$stamp.resource &> /dev/null
+
+#End of recon-ng, see workplace.timestamp for details
 
 if [ "${hostname[0]}" == "https:" ]
 then
@@ -21,19 +67,19 @@ if [ $port == 443 ] || [ $port == 80 ]
 
 then
 
-	nmap --script vuln -sV ${hostname[2]} -oA nmap/vuln_web_ports  -p $port
+	nmap --script vuln -sV $domain -oA nmap/vuln_web_ports  -p $port
 
 fi
 
 echo "Running full tcp nmap:"
 
-nmap -p- ${hostname[2]} -sV -sC --open --reason -oA nmap/full_nmap
+nmap -p- $domain -sV -sC --open --script vuln --reason -oA nmap/full_nmap
 
 searchsploit --nmap /nmap/full_nmap.xml
 
 wafw00f $1
 
-sslscan --show-certificate $1
+sslscan $1
 
 cmsmap -s $1
 
